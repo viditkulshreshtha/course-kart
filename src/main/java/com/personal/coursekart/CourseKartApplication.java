d6ca78e95d62668810da3e612bc166db3b2f0226
vkulshreshtha <viditkulshreshtha26@gmail.com>
2022-02-14 13:04:25 +0530

    Basic spring setup

---
+++ src/main/java/com/personal/coursekart/CourseKartApplication.java
@@ -0,0 +1,13 @@
+package com.personal.coursekart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/coursekart/CourseKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.coursekart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
